e2b3ebc8514f6f96e2d11d31a1db2fefa49f527f
athavan <52910050+sundaraathavan@users.noreply.github.com>
2022-06-14 03:19:43 -0700

    master

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.scss';
+import { Layout } from '../components';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
